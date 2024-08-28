d73583e364d7dd66f4ce90d3059ee141786e3ff6
Haorui-Jiang <jianghr0321@163.com>
2024-08-28 15:38:50 +0800

    Updating of web pages

---
+++ assets/js/html.js
@@ -0,0 +1,9 @@
+// 打开弹窗
+function openModal() {
+    document.getElementById("myModal").style.display = "block";
+}
+
+// 关闭弹窗
+function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+}
